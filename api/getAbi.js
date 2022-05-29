bff8818a1b65191963bf99dde8c33818ef28bd9d
bookland <bookland@porter.finance>
2022-05-29 10:03:45 -0500

    add serverless function to get abi

---
+++ api/getAbi.js
@@ -0,0 +1,16 @@
+const fetch = require('node-fetch');
+
+export default async function handler(request, response) {
+    const { address } = request.query;
+
+    // make an API call to the ABIs endpoint 
+    const result = await fetch(`https://api.etherscan.io/api?module=contract&action=getabi&address=${address}&apikey=${process.env.ETHERSCAN_API_KEY}`);
+    const data = await result.json();
+
+    // print the JSON response 
+    let abi = data.result;
+
+    response.status(200).json({
+        abi
+    });
+}
