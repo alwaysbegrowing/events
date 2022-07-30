71cc5f8860d7d890ddf63df44ba59f70d39cc074
bookland <rratcliffe57@gmail.com>
2022-07-30 13:51:34 -0500

    improve styles

--- src/App.js
+++ src/App.js
@@ -11,8 +11,9 @@ import {
   Button,
   Empty,
   PageHeader,
+  Divider, Col
 } from "antd";
-import { ContainerOutlined, Cont } from "@ant-design/icons";
+import { ContainerOutlined, GithubOutlined } from "@ant-design/icons";
 
 const { Header, Footer, Content } = Layout;
 const { Title } = Typography;
@@ -100,19 +101,27 @@ function App() {
 
   return (
     <Layout style={{ minHeight: "100vh" }}>
-      <Header >
-        <img
-          style={{
-            float: 'left',
-            height: 31,
-            margin: '16px 24px 16px 0'
-          }}
-          src="https://cleanshot-cloud-fra.s3.eu-central-1.amazonaws.com/media/34670/q3tycClOm00pMHpyE3K8iDqyHrsTALcESEAGRjHw.jpeg?X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Security-Token=IQoJb3JpZ2luX2VjENj%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaDGV1LWNlbnRyYWwtMSJGMEQCIFfJhMnkdYbInMCgETETs3gjvJpmAmSxYhrW7dV6T2CNAiAH6eg1O13y%2BenHmEdjxF5v%2BaxqYitLJjUW5dYer1PdKSqQAwgxEAAaDDkxOTUxNDQ5MTY3NCIMq71khgIl08hCmhg8Ku0Ca%2FMSvTyHRdR7D1IdaHvo3ebV8JQpGQuFTqoYVQzroQy8ufoOJdPMPqXc4X1aIyfUC%2B75CVvzSrCe8YrGz3Lzvv5rVt0rl18Jmnkd5rmCPDJPBwmeH3AccV1SSrGC6k1E7OxADYSDKeYhQfrG2YGsy1drnfodtB8dRXIQSpolCdETBm58PDLqsbsQRsdm8ZdLBl4DM8z0CWredWbTepHXN8pmENyoeJYbFhnt%2BzZNo8p%2FPCOFfnlqXt7eTpx9C72wH5PW%2Bqk1K9u%2Bwi5thHF77X2X3OcPFiUqxDMlU6hBA8jHk0O6AJ7OBQe2BuwuZA2tyll775hb5o4DxZyX%2BqWcisPILfKdBQ73rrd1BKHkdgJNFipORMtw4FGB2dtw6zpJVd7YYD41LrYWSV%2BXeQvn1Tc9PMfS%2BaEZml%2BQm5cEUM88%2B%2B32vbeBH1%2BqE1iS7YnnL2vQlJta86ZJ4NlimZ6D%2BkQZZyRPBLaXFj%2B3s3AwuJyVlwY6ngG4Dw0QzLcspVKKresDZZfZmaJ2ASIACzRdp3eIlh1uvEfW0zURlDErMexLeEeBFKKM9sP7Ot4Jo2PeFhfNlcHf7dUvshGFpLfAna%2B3rqywdMNB3vh4Cq2brLEk0zDi8RflR8yL2D1mq0IwCcGmBeuNa1D%2F7ztpakMy8YWusKuzJT6PCkOwprfoFztvoUqbxuuCJM78ExKTdKVL17btnw%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=ASIA5MF2VVMNDXQKEASQ%2F20220730%2Feu-central-1%2Fs3%2Faws4_request&X-Amz-Date=20220730T155919Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Signature=0a215ac68fd6c3b4be5c5c123acc1376ab95451c57e66a6b150ad549a12fe223"
-        />
+      <Header style={{ backgroundColor: "#f6ffed", boxShadow: "0 1px 4px rgb(0 21 41 / 8%)", zIndex: 1 }} >
+        <Row justify="space-between">
+          <Col><img
+            style={{
+              float: 'left',
+              height: 31,
+              // width: 200,
+              margin: '16px 0px 16px 0'
+            }}
+            src="https://tse3.mm.bing.net/th?id=OIP.XYaeDXspGLV6vl4xFh7CDgHaHa"
+          />
+            <b> Always Be Growing</b>
+          </Col>
+          <Col>    <Button onClick={() => { window.open('https://github.com/alwaysbegrowing/events') }} type="text"><GithubOutlined /></Button>
+          </Col>
 
+        </Row>
       </Header>
-      <PageHeader style={{ backgroundColor: "#fff" }} title='Blockchain Event Explorer'>Enter a smart contract address below to see all historic events emitted from that contract. </PageHeader>
       <Content style={{ padding: '0 24px', marginTop: 16 }}>
+        <PageHeader style={{ backgroundColor: "#fff" }} title='Blockchain Event Explorer'>Enter a smart contract address below to see all historic events emitted from that contract. </PageHeader>
+
         <div style={{ background: '#fff', padding: 24 }}>
 
           <div >
